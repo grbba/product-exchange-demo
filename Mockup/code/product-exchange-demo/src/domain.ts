34727c181cb4c3df33080b75eb6b5b0c1e60ce7a
Gregor Baues <grbaues@gmail.com>
2025-10-29 06:39:32 +0100

    Add localized product name editor

--- Mockup/code/product-exchange-demo/src/domain.ts
+++ Mockup/code/product-exchange-demo/src/domain.ts
@@ -14,6 +14,7 @@ export type Feature = {
 export type Product = {
   id: string;
   name: string;
+  localizedNames?: Record<string, string>;
   type?: string;
   lifecycleStatus: "Draft" | "Active" | "EndOfLife";
   features: Feature[];
